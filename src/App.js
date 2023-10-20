eeb504db0c2d2409eea685ed22122f2cabc57431
luhneumann <luanafneumann@hotmail.com>
2023-10-20 16:50:31 -0300

    Criando styled-components de Header e Button

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import { Home } from "../src/pages/home/index";
+
+function App() {
+  return (
+    <Home/>
+  );
+}
+
+export default App;
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,12 @@
+import { Button } from "../../components/Button"
+import { Header } from "../../components/Header"
+
+const Home = () => {
+  return(<>
+      <Header/>
+      <Button title="Teste 1"/>
+      <Button variant="secondary" title="Teste 2"/>      
+  </>)
+}
+
+export { Home }
